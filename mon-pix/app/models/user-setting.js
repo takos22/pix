6395128608bc48f527d137215b16f807823282e2
Yvonnick Frin <frin.yvonnick@gmail.com>
2022-05-09 17:20:01 +0200

    [EMBER] Start of ember bootcamp

--- mon-pix/app/models/user-setting.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class UserSetting extends Model {
-  @attr('string') color;
-  @attr('date') createdAt;
-  @attr('date') updatedAt;
-}
