010918d277b523e981bf367ae6db4ea05c3dbaeb
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-08-25 15:34:56 +0200

    feat(mon-pix): add PixSidebar in saved tutorial page
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>

--- mon-pix/tests/acceptance/user-tutorials/saved_test.js
+++ mon-pix/tests/acceptance/user-tutorials/saved_test.js
@@ -2,7 +2,8 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import { setupApplicationTest } from 'ember-mocha';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { visit, find, findAll, click } from '@ember/test-helpers';
+import { find, findAll, click } from '@ember/test-helpers';
+import { visit } from '@1024pix/ember-testing-library';
 import { authenticateByEmail } from '../../helpers/authentication';
 
 describe('Acceptance | User-tutorials-v2 | Saved', function () {
@@ -13,6 +14,7 @@ describe('Acceptance | User-tutorials-v2 | Saved', function () {
   beforeEach(async function () {
     const numberOfTutorials = 100;
     user = server.create('user', 'withEmail');
+    server.create('feature-toggle', { id: 0, isPixAppTutoFiltersEnabled: true });
     await authenticateByEmail(user);
     await server.db.tutorials.remove();
     server.createList('tutorial', numberOfTutorials, 'withUserTutorial');
@@ -40,6 +42,39 @@ describe('Acceptance | User-tutorials-v2 | Saved', function () {
 
         // then
         expect(findAll('.tutorial-card-v2')).to.be.lengthOf(9);
+      });
+    });
+  });
+
+  describe('when filter button is clicked', function () {
+    it('should open sidebar', async function () {
+      // given
+      const screen = await visit('/mes-tutos/enregistres');
+
+      // when
+      await click(screen.getByRole('button', { name: 'Filtrer' }));
+
+      // then
+      expect(find('.pix-sidebar')).to.exist;
+      expect(find('.pix-sidebar__overlay')).to.exist;
+      expect(find('.pix-sidebar__overlay--hidden')).to.not.exist;
+      expect(find('.pix-sidebar--hidden')).to.not.exist;
+    });
+  });
+
+  describe('when sidebar is open', function () {
+    describe('when close button is clicked', function () {
+      it('should close sidebar', async function () {
+        // given
+        const screen = await visit('/mes-tutos/enregistres');
+        await click(screen.getByRole('button', { name: 'Filtrer' }));
+
+        // when
+        await click(find('[aria-label="Fermer"]'));
+
+        // then
+        expect(find('.pix-sidebar__overlay--hidden')).to.exist;
+        expect(find('.pix-sidebar--hidden')).to.exist;
       });
     });
   });
