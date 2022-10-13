35c540e24d07af34e4a8c62a7aa35b764c6df00b
Vincent Hardouin <vincent.hardouin@pix.fr>
2022-10-13 18:05:34 +0200

    feat(mon-pix): create new training card
    Co-authored-by: Clarisse Damon <clarisse.damon@pix.fr>
    Co-authored-by: Matthias Ferraina <matthias.ferraina@pix.fr>

--- mon-pix/app/components/training/card.js
+++ mon-pix/app/components/training/card.js
@@ -4,11 +4,40 @@ import { inject as service } from '@ember/service';
 export default class Card extends Component {
   @service intl;
   get durationFormatted() {
-    const hours = this.args.training.duration.hours + 'h';
-    return hours;
+    const formattedHours = [];
+    const { hours, minutes, seconds } = this.args.training.duration;
+    if (hours) {
+      formattedHours.push(hours + 'h');
+    }
+    if (minutes) {
+      formattedHours.push(minutes + 'm');
+    }
+    if (seconds) {
+      formattedHours.push(seconds + 's');
+    }
+    return formattedHours.join(' ');
   }
 
   get type() {
     return this.intl.t(`pages.training.type.${this.args.training.type}`);
+  }
+
+  get tagColor() {
+    if (this.args.training.isAutoformation) {
+      return 'blue-light';
+    }
+    return 'purple-light';
+  }
+
+  get imageSrc() {
+    const randomNumber = this._getRandomImageNumber();
+    if (this.args.training.isAutoformation) {
+      return `/images/illustrations/trainings/Illu_Parcours_autoformation-${randomNumber}.jpg`;
+    }
+    return `/images/illustrations/trainings/Illu_Webinaire-${randomNumber}.jpg`;
+  }
+
+  _getRandomImageNumber() {
+    return Math.floor(Math.random() * (3 - 1) + 1);
   }
 }
