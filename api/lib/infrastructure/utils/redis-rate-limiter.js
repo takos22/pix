ab2ad7212d34dbcbd5755b13e1da794cdde9b505
François de Metz <francois.de-metz@pix.fr>
2022-10-31 12:43:09 +0100

    Update redis rate limiter

--- api/lib/infrastructure/utils/redis-rate-limiter.js
+++ api/lib/infrastructure/utils/redis-rate-limiter.js
@@ -1,13 +1,13 @@
 const redis = require('redis');
-const { promisify } = require('util');
 const logger = require('../logger');
 const settings = require('../../config');
 const redisRateLimiter = settings.rateLimit.redisUrl ? buildRedisRateLimiter() : null;
 
 function buildRedisRateLimiter() {
   const rateLimiter = redis.createClient(settings.rateLimit.redisUrl);
-  rateLimiter.scriptAsync = promisify(rateLimiter.script).bind(rateLimiter);
-  rateLimiter.evalshaAsync = promisify(rateLimiter.evalsha).bind(rateLimiter);
+  rateLimiter.connect();
+  rateLimiter.scriptAsync = rateLimiter.scriptLoad.bind(rateLimiter);
+  rateLimiter.evalshaAsync = rateLimiter.evalSha.bind(rateLimiter);
   rateLimiter.on('error', (err) => logger.warn({ redisClient: 'rate-limit', err }, 'Error encountered'));
   return rateLimiter;
 }
