1211a0be1a2523d75b573bbf06a02dfead50ad1e
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-11-29 15:41:07 +0100

    create user-settings route handler

---
+++ mon-pix/app/routes/user-settings.js
@@ -0,0 +1,4 @@
+import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
+import Route from '@ember/routing/route';
+
+export default class UserSettingsRoute extends Route.extend(SecuredRouteMixin) {}
