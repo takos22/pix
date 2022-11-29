bd437e7d114eabe0f0dd5079e5a58e96a9258bc2
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-11-29 15:55:59 +0100

    return user-setting model for current user id

--- mon-pix/app/routes/user-settings.js
+++ mon-pix/app/routes/user-settings.js
@@ -1,4 +1,12 @@
 import SecuredRouteMixin from 'mon-pix/mixins/secured-route-mixin';
 import Route from '@ember/routing/route';
+import { inject as Service } from '@ember/service';
 
-export default class UserSettingsRoute extends Route.extend(SecuredRouteMixin) {}
+export default class UserSettingsRoute extends Route.extend(SecuredRouteMixin) {
+  @Service currentUser;
+
+  async model() {
+    const userSettings = await this.store.findRecord('user-setting', this.currentUser.user.id);
+    return { userSettings };
+  }
+}
