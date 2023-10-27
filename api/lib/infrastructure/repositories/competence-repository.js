36b4ded2b8b8f408cc2d5728cd4d259014f0e95c
Antoine Ceol <antoine.ceol@gmail.com>
2023-10-27 17:13:05 +0200

    :recycle: api: Move locals constants to src

--- api/lib/infrastructure/repositories/competence-repository.js
+++ api/lib/infrastructure/repositories/competence-repository.js
@@ -3,7 +3,8 @@ import { LearningContentResourceNotFound } from '../datasources/learning-content
 import { Competence } from '../../domain/models/Competence.js';
 import { competenceDatasource } from '../datasources/learning-content/competence-datasource.js';
 import { NotFoundError } from '../../domain/errors.js';
-import { LOCALE, PIX_ORIGIN } from '../../domain/constants.js';
+import { PIX_ORIGIN } from '../../domain/constants.js';
+import { LOCALE } from '../../../src/shared/domain/constants.js';
 
 const { FRENCH_FRANCE } = LOCALE;
 
